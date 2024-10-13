549ed4967c4cb7f9d637cdcc38f8e3a17ba309a6
CatHead132 <116957800+CatHead132@users.noreply.github.com>
2024-10-12 21:51:19 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/class-schedule-app/', // Replace with your repository name
+  base: '/classes/', // Replace with your repository name
 });
